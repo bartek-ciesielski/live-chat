95ef2b43e21ee31807ebff4ddbacbbf07825df9b
Bartek Ciesielski <bartek.ciesielski@renderro.com>
2021-08-08 21:14:39 +0200

    feat: add watcher fot the user

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-// import global css
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+// import firebase auth service
+import { projectAuth } from './firebase/config'
+
+let app
+
+projectAuth.onAuthStateChanged(() => {
+    if (!app) {
+        app = createApp(App)
+            .use(router)
+            .mount('#app')
+    }
+})
